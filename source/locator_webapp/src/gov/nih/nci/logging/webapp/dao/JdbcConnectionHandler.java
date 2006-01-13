10855e65195852f6d377df9dcd87522871eb1716
Yongjian Lian <liany@mail.nih.gov>
2006-01-13 18:46:29 +0000

    *** empty log message ***
    SVN-Revision: 31

--- source/locator_webapp/src/gov/nih/nci/logging/webapp/dao/JdbcConnectionHandler.java
+++ source/locator_webapp/src/gov/nih/nci/logging/webapp/dao/JdbcConnectionHandler.java
@@ -7,29 +7,27 @@ package gov.nih.nci.logging.webapp.dao;
  * <!-- LICENSE_TEXT_END -->
  */
 import java.sql.*;
-import java.util.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 /**
- * Utility Class for obtaining a JDBC connection. This class is configurable by
- * modifying the connection.properties in the resources.
+ * Utility Class for obtaining a JDBC connection. This class looks up database connection
+ * with the mysql-ds.xml.
  * 
  * @author Ekagra Software Technologies Limited ('Ekagra')
  * 
  */
 public class JdbcConnectionHandler
 {
-
+	public final static String dbName = "java:/MySqlDSLogLocator";
+	public static DataSource  dbSource = null;
 	static
 	{
 		try
-		{
-			// load the jdbc properties
-			Properties p = new Properties();
-			p.load(SummaryDaoJdbc.class.getResourceAsStream("/resources/connection.properties"));
-			setDbDriverClass(p.getProperty("databaseDriverName"));
-			setDbUser(p.getProperty("databaseUserName"));
-			setDbPwd(p.getProperty("databasePassword"));
-			setDbUrl(p.getProperty("databaseURL"));
+		{			
+			Context ctx = new InitialContext();
+			dbSource = (DataSource)ctx.lookup(dbName);			 								 
 		}
 		catch (Exception ex)
 		{
@@ -38,10 +36,6 @@ public class JdbcConnectionHandler
 
 	}
 
-	private static String dbDriverClass;
-	private static String dbUser;
-	private static String dbPwd;
-	private static String dbUrl;
 
 	/**
 	 * Creates a new JDBC connection.
@@ -51,80 +45,8 @@ public class JdbcConnectionHandler
 	 */
 	public static Connection createConnection() throws Exception
 	{
-
-		if (getDbDriverClass() != null)
-		{
-			Class.forName(getDbDriverClass());
-		}
-		return DriverManager.getConnection(getDbUrl(), getDbUser(), getDbPwd());
-
+		  return  dbSource.getConnection();	
 	}
 
-	/**
-	 * @return Returns the dbDriverClass.
-	 */
-	public static String getDbDriverClass()
-	{
-		return dbDriverClass;
-	}
-
-	/**
-	 * @param dbDriverClass
-	 * The dbDriverClass to set.
-	 */
-	public static void setDbDriverClass(String dbDriverClass)
-	{
-		JdbcConnectionHandler.dbDriverClass = dbDriverClass;
-	}
-
-	/**
-	 * @return Returns the dbPwd.
-	 */
-	public static String getDbPwd()
-	{
-		return dbPwd;
-	}
-
-	/**
-	 * @param dbPwd
-	 * The dbPwd to set.
-	 */
-	public static void setDbPwd(String dbPwd)
-	{
-		JdbcConnectionHandler.dbPwd = dbPwd;
-	}
-
-	/**
-	 * @return Returns the dbUrl.
-	 */
-	public static String getDbUrl()
-	{
-		return dbUrl;
-	}
-
-	/**
-	 * @param dbUrl
-	 * The dbUrl to set.
-	 */
-	public static void setDbUrl(String dbUrl)
-	{
-		JdbcConnectionHandler.dbUrl = dbUrl;
-	}
-
-	/**
-	 * @return Returns the dbUser.
-	 */
-	public static String getDbUser()
-	{
-		return dbUser;
-	}
-
-	/**
-	 * @param dbUser
-	 * The dbUser to set.
-	 */
-	public static void setDbUser(String dbUser)
-	{
-		JdbcConnectionHandler.dbUser = dbUser;
-	}
+	
 }
