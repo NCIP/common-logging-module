7e94ed7655c4dc31253c1444f3dd440de5638e06
Vijay Parmar <parmarv@mail.nih.gov>
2006-08-22 13:45:51 +0000

    Committing version 3.2 changes.
    SVN-Revision: 73

---
+++ source/locator_webapp/public_html/WEB-INF/pages/body/querymain.jsp
@@ -0,0 +1,5 @@
+<tr>
+	<td width="100%" valign="top">Query Page ( without results)..show
+	message asking user to populate search criteria and click submit to
+	start searching. More information as required.</td>
+</tr>
---
+++ source/locator_webapp/src/gov/nih/nci/logging/webapp/form/LoginForm.java
@@ -0,0 +1,107 @@
+//Created by MyEclipse Struts
+// XSL source (default): platform:/plugin/com.genuitec.eclipse.cross.easystruts.eclipse_4.0.0/xslt/JavaClass.xsl
+
+package gov.nih.nci.logging.webapp.form;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+/** 
+ * MyEclipse Struts
+ * Creation date: 08-18-2006
+ * 
+ * XDoclet definition:
+ * @struts.form name="loginForm"
+ */
+public class LoginForm extends ActionForm {
+
+	// --------------------------------------------------------- Instance Variables
+
+	/** password property */
+	private String password;
+
+	/** username property */
+	private String username;
+
+	/** application property */
+	private String application;
+
+	// --------------------------------------------------------- Methods
+
+	/** 
+	 * Method validate
+	 * @param mapping
+	 * @param request
+	 * @return ActionErrors
+	 */
+	public ActionErrors validate(
+		ActionMapping mapping,
+		HttpServletRequest request) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** 
+	 * Method reset
+	 * @param mapping
+	 * @param request
+	 */
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+
+		// TODO Auto-generated method stub
+	}
+
+	/** 
+	 * Returns the password.
+	 * @return String
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/** 
+	 * Set the password.
+	 * @param password The password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/** 
+	 * Returns the username.
+	 * @return String
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/** 
+	 * Set the username.
+	 * @param username The username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/** 
+	 * Returns the application.
+	 * @return String
+	 */
+	public String getApplication() {
+		return application;
+	}
+
+	/** 
+	 * Set the application.
+	 * @param application The application to set
+	 */
+	public void setApplication(String application) {
+		this.application = application;
+	}
+
+}
+
