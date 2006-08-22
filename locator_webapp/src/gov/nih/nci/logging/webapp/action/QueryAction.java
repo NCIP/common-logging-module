f3a68b0f2ae83b748c52b8e627cb5d9103a4993d
Vijay Parmar <parmarv@mail.nih.gov>
2006-08-22 13:45:51 +0000

    Committing version 3.2 changes.
    SVN-Revision: 73

---
+++ locator_webapp/src/gov/nih/nci/logging/webapp/action/QueryAction.java
@@ -0,0 +1,45 @@
+//Created by MyEclipse Struts
+// XSL source (default): platform:/plugin/com.genuitec.eclipse.cross.easystruts.eclipse_4.0.0/xslt/JavaClass.xsl
+
+package gov.nih.nci.logging.webapp.action;
+
+import gov.nih.nci.logging.webapp.form.LoginForm;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/** 
+ * Creation date: 08-18-2006
+ */
+public class QueryAction extends Action {
+
+	// --------------------------------------------------------- Instance Variables
+
+	// --------------------------------------------------------- Methods
+
+	/** 
+	 * Method execute
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return ActionForward
+	 */
+	public ActionForward execute(
+		ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response) {
+		LoginForm loginForm = (LoginForm) form;
+		// TODO Auto-generated method stub
+		//return mapping.findForward("showQuery");
+		return mapping.findForward("showQueryResults");
+	}
+
+}
+
