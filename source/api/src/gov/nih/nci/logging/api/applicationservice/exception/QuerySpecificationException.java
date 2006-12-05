f95d54d7c3fe9c12b5ca7c3725faf377504ef133
Vijay Parmar <parmarv@mail.nih.gov>
2006-12-05 18:17:12 +0000

    Improved JavaDocs
    SVN-Revision: 197

--- source/api/src/gov/nih/nci/logging/api/applicationservice/exception/QuerySpecificationException.java
+++ source/api/src/gov/nih/nci/logging/api/applicationservice/exception/QuerySpecificationException.java
@@ -5,6 +5,8 @@ package gov.nih.nci.logging.api.applicationservice.exception;
 
 /**
  * Query Specification Exception.
+ * 
+ * @author Vijay Parmar (Ekagra Software Technologies Limited.)
  *
  */
 public class QuerySpecificationException extends Exception {
--- source/api/src/gov/nih/nci/logging/api/applicationservice/exception/SearchCriteriaSpecificationException.java
+++ source/api/src/gov/nih/nci/logging/api/applicationservice/exception/SearchCriteriaSpecificationException.java
@@ -3,6 +3,10 @@ package gov.nih.nci.logging.api.applicationservice.exception;
 
 
 
+/**
+ * @author Vijay Parmar (Ekagra Software Technologies Limited.)
+ *
+ */
 public class SearchCriteriaSpecificationException extends Exception {
 	 /**
 	  * Constructor for SearchCriteriaSpecificationException. 
