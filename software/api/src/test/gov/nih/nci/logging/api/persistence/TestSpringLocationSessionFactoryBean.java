a43e293c908e383dd70d6f759499a4948fbd55b9
Prasad Konka <prasad.konka@nih.gov>
2011-05-24 16:47:58 +0000

    Changes for 2011 current tech stack upgrade
    SVN-Revision: 323

--- software/api/src/test/gov/nih/nci/logging/api/persistence/TestSpringLocationSessionFactoryBean.java
+++ software/api/src/test/gov/nih/nci/logging/api/persistence/TestSpringLocationSessionFactoryBean.java
@@ -22,15 +22,15 @@ public class TestSpringLocationSessionFactoryBean {
 		java.io.InputStream in = FileLoader.getInstance().getFileAsStream("commons-logging.properties");
 		Resource resource = new InputStreamResource(in);
 		System.out.println(resource.exists());
-		
+
 		LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-		String mappingResources[] = 
+		String mappingResources[] =
 		{"gov/nih/nci/logging/api/domain/LogMessage.hbm.xml","gov/nih/nci/logging/api/domain/ObjectAttribute.hbm.xml"};
 		localSessionFactoryBean.setMappingResources(mappingResources);
 		BasicDataSource ds= setDataSourceProperties();
 		localSessionFactoryBean.setDataSource(ds);
 		localSessionFactoryBean.setHibernateProperties(getJDBCProperties());
-		
+
 		try {
 			localSessionFactoryBean.afterPropertiesSet();
 		} catch (HibernateException e) {
@@ -42,7 +42,10 @@ public class TestSpringLocationSessionFactoryBean {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		  SessionFactory sf = (SessionFactory) localSessionFactoryBean.getObject();
 		  //SessionFactory sessionFactory = (SessionFactory) ObjectFactory.getObject(ObjectFactory.HibernateSessionFactory);
 	}
@@ -50,22 +53,22 @@ public class TestSpringLocationSessionFactoryBean {
 		Properties props = new Properties();
         props.setProperty("hibernate.connection.driver_class","com.mysql.jdbc.Driver");
         props.setProperty("hibernate.connection.url","jdbc:mysql://localhost:3306/clm");
-        props.setProperty("hibernate.connection.username","root");	
+        props.setProperty("hibernate.connection.username","root");
         props.setProperty("hibernate.connection.password","admin");
         props.setProperty("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
         props.setProperty("jdbc.batch_size","30");
         return props;
 	}
-	
+
 	private static BasicDataSource setDataSourceProperties(){
 		BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/clm");
-        ds.setUsername("root");	
+        ds.setUsername("root");
         ds.setPassword("admin");
         /*props.setProperty("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
         props.setProperty("jdbc.batch_size","30");*/
         return ds;
 	}
-	
+
 }
