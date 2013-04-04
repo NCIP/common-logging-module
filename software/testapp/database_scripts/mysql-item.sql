639ace2b55bfcc13a430b60a86d8bf4e4ece938b
Prasad Konka <prasad.konka@nih.gov>
2013-04-04 17:54:09 -0400

    Update copyright

--- software/testapp/database_scripts/mysql-item.sql
+++ software/testapp/database_scripts/mysql-item.sql
@@ -1,3 +1,10 @@
+/*L
+   Copyright Ekagra Software Technologies
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/common-logging-module/LICENSE.txt for details.
+L*/
+
 create table item (id int(11) not null primary key,
 name varchar(255),
 manufacturer varchar(255),
